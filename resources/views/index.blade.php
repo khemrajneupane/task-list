e48a6ff90997447c3702c67716d0f4cc9e27c882
Khem Raj Neupane <khemrajneupane@Khems-MacBook-Pro.local>
2025-04-06 22:02:40 +0300

    new commo layout template view adds, template inheirtance happens with section direction for content and title in show.blade.php and index.blade.php files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,7 @@
-{{-- @isset($name)
-    Hello {{ $name }} !
-@endisset
+@extends('layouts.app')
 
-<div>
-    hello from blade template
-</div>
-
-<div>
-
-    <h1>Tasks lists:</h1>
-    @forelse ($tasks as $task)
-        <p>{{ $task->title }}</p>
-    @empty
-        <div>No tasks </div>
-    @endforelse
-
-
-    <div>There are no tasks</div>
-  
-
-</div> --}}
-
-<div> Tasks </div>
-<div>
+@section('title', 'List of Tasks')
+@section('content')
     @forelse ($tasks as $task)
         <div>
             <a href="{{ route('task.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
@@ -30,4 +9,4 @@
     @empty
         <div>No tasks </div>
     @endforelse
-</div>
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,1 +1,11 @@
-<div>Hello from show.blade.php file </div>
+@extends('layouts.app')
+
+@section('title', $task->title)
+@section('content')
+    <h1>{{ $task->description }}</h1>
+    @if ($task->long_description)
+        <p>{{ $task->long_description }}</p>
+    @endif
+    <p>{{ $task->created_at }}</p>
+    <p>{{ $task->updated_at }}</p>
+@endsection
